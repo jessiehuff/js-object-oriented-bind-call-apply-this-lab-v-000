aef9a16902f5804ef7d9f04f55c67a54d8fd5031
Jessie Huff <huffjd1@gmail.com>
2018-03-08 19:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ function setThisWithCall(fn, thisValue, arg){
 function setThisWithApply(fn, thisValue, args){
   return fn.apply(thisValue, args)
 }
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  return 
+}
